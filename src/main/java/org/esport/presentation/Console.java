1aa1749fd30b4e448adcd4dab1c99c1f7c1b25ab
Naoufal <naoufal.labrihmi@gmail.com>
2024-10-26 13:22:02 +0100

    Update Console.java

--- src/main/java/org/esport/presentation/Console.java
+++ src/main/java/org/esport/presentation/Console.java
@@ -13,7 +13,7 @@ public class Console {
         LOGGER.info("Démarrage de l'application");
         ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
         org.h2.tools.Server h2WebServer = context.getBean("h2WebServer", org.h2.tools.Server.class);
-        String h2ConsoleUrl = h2WebServer.getURL();
+        String h2ConsoleUrl = "http://localhost:" + h2WebServer.getPort() + "/";
         LOGGER.info("H2 Console URL: {}", h2ConsoleUrl);
         MainMenu mainMenu = context.getBean(MainMenu.class);
         mainMenu.afficherMenuPrincipal();
